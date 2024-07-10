4e69efc7a28931518967d90c40975c0989d8782e
Xiao Xue <xxue8761@conestogac.on.ca>
2024-07-09 20:50:12 -0400

    Don't allow no lot being selected prior to registry.

--- src/Component/AcceptVehicleForm.jsx
+++ src/Component/AcceptVehicleForm.jsx
@@ -2,28 +2,39 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from 'react-bootstrap/Form'
 import {useState} from "react";
-import {Lot} from "./Lot";
+import '../App.css';
 
 function SingleLot (props) {
     const {lots, show, handleClose, registerVehicle} = props;
     const [plateNumber, setPlateNumber] = useState('');
-    const [selectedLot, setSelectedLot] = useState('');
+    const [selectedLot, setSelectedLot] = useState(null);
+    const [invalid, setInvalid] = useState(false);
 
     const handlePlateNumberChange = (e) => setPlateNumber(e.target.value);
-    const handleLotChange = (e) => setSelectedLot(e.target.value);
+    const handleLotChange = (e) => {
+        if (e.target.value !== null) {
+            setInvalid(false);
+        }
+        setSelectedLot(e.target.value)
+    };
 
     const submitRegistration = () => {
+        if (!selectedLot) {
+            setInvalid(true);
+            return;
+        }
+
         let parkedVehicle = {
             lot: selectedLot,
             plate : plateNumber,
             parkedTime: new Date(),
         }
         console.log(parkedVehicle);
-        debugger;
         registerVehicle(parkedVehicle);
 
         setPlateNumber("");
         setSelectedLot("");
+        setInvalid(false);
         handleClose();
     }
 
@@ -45,12 +56,15 @@ function SingleLot (props) {
                       <Form.Control type="text" onChange={handlePlateNumberChange} placeholder={"Enter license plate"}/>
                   </Form.Group>
                   <Form.Group className={"mt-4"}>
-                      <Form.Select onChange={handleLotChange}>
-                          <option>Allocate lot to vehicle</option>
+                      <Form.Select onChange={handleLotChange} required>
+                          <option defaultChecked={true} value={null}>Allocate lot to vehicle</option>
                           {
                             lots.map ((lot, index) => <option disabled={lot.occupied} value={index + 1}>{index + 1}</option>)
                           }
                       </Form.Select>
+                      {
+                          invalid && <Form.Text> Must select a valid lot ! </Form.Text>
+                      }
                   </Form.Group>
                 </Form>
             </Modal.Body>
