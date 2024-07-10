5e34100cdcab801bd24939127a69d43752d525d9
Xiao Xue <xxue8761@conestogac.on.ca>
2024-07-10 19:14:13 -0400

    update the display of elapsed time per second.

--- src/Component/SingleLot.jsx
+++ src/Component/SingleLot.jsx
@@ -1,19 +1,37 @@
 import Button from "react-bootstrap/Button";
 import ReleaseVehicleForm from "./ReleaseVehicleForm";
 import dayjs from "dayjs";
+import {useEffect, useState} from "react";
 
 const relativeTime = require("dayjs/plugin/relativeTime");
 const duration = require('dayjs/plugin/duration');
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
 
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 function SingleLot (props) {
     const {showReleaseForm, lot, openForm, handleClose} = props
 
+    const [timeElapsed, setTimeElapsed] = useState(dayjs().diff(lot.entryTime, 'second'));
+
+    useEffect(() => {
+        if (showReleaseForm){
+            return;
+        }
+        const interval = setInterval(() => {
+            setTimeElapsed(dayjs().diff(lot.entryTime, 'second'));
+        }, 1000)
+
+        return () => clearInterval(interval);
+    }, [lot.entryTime, showReleaseForm]);
+
     let status = lot.occupied ? "Not Empty" : "Empty";
 
-    const timeElapsed = dayjs().diff(lot.entryTime, 'second');
+    // const timeElapsed = dayjs().diff(lot.entryTime, 'second');
     lot.duration =  dayjs.duration(timeElapsed, 'seconds').format('m[min] s[seconds]');
 
     const segment = Math.floor(timeElapsed / 30);
@@ -22,9 +40,10 @@ function SingleLot (props) {
     return (
         <div>
             <h2>{lot.lotNumber}</h2>
-            <p>{lot.plateNumber}</p>
-            {lot.entryTime && <p>{lot.entryTime.toLocaleString()}</p>}
             <p>{status}</p>
+            <p>{lot.plateNumber}</p>
+            {lot.entryTime && <p>{dayjs(lot.entryTime).tz("America/Toronto").format('YYYY-MM-DD HH:mm:ss')}</p>}
+            {lot.occupied && <p>{lot.duration}</p>}
 
             <br/>
             {
